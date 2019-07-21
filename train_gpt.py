1e92bf638f594c41dedd48fb71d697398b946d74
Ubuntu <ubuntu@ip-172-31-41-129.ec2.internal>
2019-07-21 22:07:11 +0000

    Samples

--- train_gpt.py
+++ train_gpt.py
@@ -5,12 +5,11 @@ base_path = os.path.dirname(os.path.realpath(__file__))
 
 #gpt2.download_gpt2(model_name='345M')   # model is saved into current directory under /models/117M/
 
-name = "andrewyang"
+name = "Ian"
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               base_path + '/data/{}.txt'.format(name),
               model_name='345M',
               steps=2000, run_name=name)   # steps is max number of training steps
 
-gpt2.load_gpt2(sess, run_name=name)
 gpt2.generate(sess, run_name=name)
