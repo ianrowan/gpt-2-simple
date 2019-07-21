a4d4509dfd7b41695dfb87389f75e27fc3d4554e
ian <i.rowan299@gmail.com>
2019-07-21 13:20:29 -0500

    new data

--- train_gpt.py
+++ train_gpt.py
@@ -3,11 +3,11 @@ import os
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 
-gpt2.download_gpt2(model_name='345M')   # model is saved into current directory under /models/117M/
+#gpt2.download_gpt2(model_name='345M')   # model is saved into current directory under /models/117M/
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
-              '{base_path}/data/elon1.csv',
+              '{base_path}/data/elon2.txt',
               model_name='345M',
               steps=10000)   # steps is max number of training steps
 
