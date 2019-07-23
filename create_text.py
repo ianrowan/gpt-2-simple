693a4efe761b4fc6e3239f01604778cc114cc71c
ian <i.rowan299@gmail.com>
2019-07-22 19:47:31 -0500

    Added Data

--- create_text.py
+++ create_text.py
@@ -6,5 +6,11 @@ base_path = os.path.dirname(os.path.realpath(__file__))
 prompt = input("Write a few words or click enter for random: ")
 
 sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, run_name='run1')
-gpt2.generate(sess, run_name='run1', prefix=prompt if prompt is not ""else None)
+gpt2.load_gpt2(sess, run_name='Ian')
+gpt2.generate(sess, run_name='Ian', prefix=prompt if prompt is not ""else None)
+'''
+output = gpt2.generate(sess, run_name='andrewyang', prefix=prompt if prompt is not ""else None, return_as_list=True, nsamples=5)
+
+for i in range(len(output)):
+    print(output[i].split("\n")[0])
+'''
--- train_gpt.py
+++ train_gpt.py
@@ -5,7 +5,7 @@ base_path = os.path.dirname(os.path.realpath(__file__))
 
 #gpt2.download_gpt2(model_name='345M')   # model is saved into current directory under /models/117M/
 
-name = "Ian"
+name = "Reid"
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               base_path + '/data/{}.txt'.format(name),
